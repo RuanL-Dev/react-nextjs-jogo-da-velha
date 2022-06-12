fb6b5cdff71b8da8cfcaeb3af374902e0a530613
Ruan Lopes Gomes <ruanlopes21@gmail.com>
2022-06-12 15:43:05 -0300

    Criado componente de ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,13 +2,16 @@ import styles from './Header.module.css'
 
 import Title from '../title/Title'
 import Subtitle from '../subtitle/Subtitle'
-
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
       <Subtitle>Criado por Ruan Gomes</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link="https://github.com/RuanL-Dev"/>
+      </div>
     </div>
   )
 }
